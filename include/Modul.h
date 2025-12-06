0ce9616c327bcdccdf669ff2443834fe678fd741
kajjtek <kaj.plw24@gmail.com>
2025-12-06 17:05:33 +0100

    fix

--- include/Modul.h
+++ include/Modul.h
@@ -1,13 +1,14 @@
 #ifndef MODUL_H
 #define MODUL_H
 
+#include "./View.h"
 #include <pcl/point_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/filters/extract_indices.h>
 
 class Modul {
     public:
-    virtual void detect(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_no_ground);
+    virtual void detect(View& view);
 };
 
 #endif
